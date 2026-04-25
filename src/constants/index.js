002d30dee0a92f90a738c8a8fe3f59362b387f0b
Sumit <sumit9229krsingh@gmail.com>
2026-04-25 21:58:10 +0530

    build all section of the website

---
+++ src/constants/index.js
@@ -0,0 +1,178 @@
+// index.js
+export const servicesData = [
+  {
+    title: "FullStack Development",
+    description:
+      "Your business deserves a fast, secure, and future-proof digital foundation. I develop custom web apps with clean architecture, optimized databases, and seamless integrations—ensuring reliability at every layer.",
+    items: [
+      {
+        title: "Backend Engineering",
+        description: "(REST/GraphQL APIs, Microservices, Auth Systems)",
+      },
+      {
+        title: "Frontend Excellence",
+        description: "(React, Vue, TypeScript, Interactive UI/UX)",
+      },
+      {
+        title: "Database Design",
+        description: "(SQL/NoSQL Optimization, Scalable Structures)",
+      },
+    ],
+  },
+  {
+    title: "DevOps & Cloud Solutions",
+    description:
+      "Deploying software shouldn't be a gamble. I automate infrastructure, enforce security, and leverage cloud platforms (AWS/Azure) to keep your app running smoothly—24/7, at any scale.",
+    items: [
+      {
+        title: "CI/CD Pipelines",
+        description: "(GitHub Actions, Docker, Kubernetes)",
+      },
+      {
+        title: "Server Management ",
+        description: "(Linux, Nginx, Load Balancing)",
+      },
+      {
+        title: "Performance Tuning",
+        description: "(Caching, Compression, Lighthouse 90+ Scores)",
+      },
+    ],
+  },
+  {
+    title: "Security & Optimization",
+    description:
+      "Slow or hacked apps destroy trust. I harden security (XSS/SQLI protection, OAuth) and optimize bottlenecks so your app stays fast, safe, and scalable as you grow.",
+    items: [
+      {
+        title: "Code Audits",
+        description: "(Refactoring, Tech Debt Cleanup)",
+      },
+      {
+        title: "Pen Testing",
+        description: "(Vulnerability Assessments)",
+      },
+      {
+        title: "SEO Tech Stack",
+        description: "(SSR, Metadata, Structured Data)",
+      },
+    ],
+  },
+  {
+    title: "Web & Mobile Apps",
+    description:
+      "A clunky interface can sink even the best ideas. I craft responsive, pixel perfect web and mobile apps (React Native/Flutter) that users love—bridging design and functionality seamlessly.",
+    items: [
+      {
+        title: "Cross-Platform Apps",
+        description: "(Single codebase for iOS/Android/Web)",
+      },
+      {
+        title: "PWAs",
+        description: "(Offline mode, Push Notifications)",
+      },
+      {
+        title: "E-Commerce",
+        description: "(Checkout flows, Payment Gateways, Inventory APIs)",
+      },
+    ],
+  },
+];
+export const projects = [
+  {
+    id: 1,
+    name: "Software engineering platform",
+    description:
+      "Provn is the software engineering platform that bridges the gap betweencampus codingandtech hiring.",
+    href: "https://www.provn.live/",
+    image: "/images/Provn.webp",
+    bgImage: "/images/Provn.webp",
+    frameworks: [
+      { id: 1, name: "Next.js" },
+      { id: 2, name: "PostgreSQL" },
+      { id: 3, name: "Node.js" },
+      { id: 4, name: "Express.js" },
+      { id: 5, name: "Tailwind CSS" },
+    ],
+  },
+  {
+    id: 2,
+    name: "Modern gym website",
+    description:
+      "An online store specializing in rare and decorative plants with a clean, user-friendly interface.",
+    href: "https://www.onerepmaaxgym.in/",
+    image: "/images/GYM.webp",
+    bgImage: "/images/GYM.webp",
+    frameworks: [
+      { id: 1, name: "Express.js" },
+      { id: 2, name: "React.js" },
+      { id: 3, name: "MongoDB" },
+      { id: 4, name: "Tailwind CSS" },
+    ],
+  },
+  {
+    id: 3,
+    name: "Expert counseling",
+    description:
+      "An e-commerce platform for Apple products and accessories with deals and category filtering.",
+    href: "https://www.ramaeducare.in/",
+    image: "/images/RAMA.webp",
+    bgImage: "/images/RAMA.webp",
+    frameworks: [
+      { id: 1, name: "Express.js" },
+      { id: 2, name: "Next.js" },
+      { id: 3, name: "MongoDB" },
+      { id: 4, name: "Tailwind CSS" },
+    ],
+  },
+  {
+    id: 4,
+    name: "Game Platform",
+    description:
+      "A multi-category online shop featuring electronics, home appliances, and gaming gear with special offers.",
+    href: "https://zentry-dun.vercel.app/",
+    image: "/images/Game.webp",
+    bgImage: "/images/Game.webp",
+    frameworks: [
+      { id: 1, name: "Next.js" },
+      { id: 2, name: "Gsap" },
+      { id: 3, name: "Motion" },
+      { id: 4, name: "Tailwind CSS" },
+    ],
+  },
+  {
+    id: 5,
+    name: "Collabration Platform",
+    description:
+      "A curated collection of designer home decor items, including furniture and artisan vases.",
+    href: "https://collabase.vercel.app/",
+    image: "/images/Collab.webp",
+    bgImage: "/images/Collab.webp",
+    frameworks: [
+      { id: 1, name: "React.js" },
+      { id: 2, name: "Firebase" },
+      { id: 3, name: "GraphQL" },
+      { id: 4, name: "Material UI" },
+    ],
+  },
+  {
+    id: 6,
+    name: "Hackathon Registration Platform for College",
+    description:
+      "A gaming platform featuring discounted titles, top sellers, and genre-based browsing.",
+    href: "https://hackhorizon.online/",
+    image: "/images/Hack.webp",
+    bgImage: "/images/Hack.webp",
+    frameworks: [
+      { id: 1, name: "Svelte" },
+      { id: 2, name: "Node.js" },
+      { id: 3, name: "MongoDB" },
+      { id: 4, name: "Chakra UI" },
+    ],
+  },
+];
+export const socials = [
+  { name: "Instagram", href: "https://www.instagram.com/zenux.dev/" },
+ 
+  { name: "LinkedIn", href: "https://www.linkedin.com/in/ali-sanati/" },
+ 
+];
